395b12b9fa7c559eede16ddcd411be6fa95812c8
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-27 19:18:21 -0600

    Add initial class structure & basic render

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,4 +1,17 @@
 import React, { Component } from 'react';
 import './Search.css';
 
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h3>Search goes here</h3>
+    )
+  }
+}
+
 export default Search;
