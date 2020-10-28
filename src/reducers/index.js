8ed12c1ca2c11b3ff2156738eaabb1c588b4c8c8
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-27 20:58:58 -0600

    Add import/export for combineReducers, create allReducers constant

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+//will import all reducers here as their own import line
+
+const allReducers = combineReducers({
+  //w/e name you want: import name
+})
+
+export default allReducers;
