63c47883aca08695960df037a59b47ae72be99f2
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-27 19:10:06 -0600

    Create App folder for component, render h1 to make sure files hooked up

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h1>Bibliogoal</h1>
+    )
+  }
+}
+
+export default App;
