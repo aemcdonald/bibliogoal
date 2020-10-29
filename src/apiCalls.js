ddd820a42e86522cff9c3764de0028e46e7c4d6f
Stacy Potten <stacylynnaz@gmail.com>
2020-10-29 15:10:29 -0600

    Add interpolated genre to fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
-export const getBooks = () => {
-  return fetch('https://api.nytimes.com/svc/books/v3/lists/current/Hardcover Fiction.json?api-key=8XyEaxrna31JNzWjF8OLgl3naZbdWN91')
+export const getBooks = (listName) => {
+  return fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${listName}.json?api-key=8XyEaxrna31JNzWjF8OLgl3naZbdWN91`)
   .then(response => {
     if(!response.ok) {
       throw Error("failed to retrieve books")
