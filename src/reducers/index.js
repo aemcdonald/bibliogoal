746efda45d13583362da7b1c71d475e91c3553ed
Naomi Ware <nware1066@yahoo.com>
2020-10-29 10:47:19 -0600

    Add books reducer to allReducers file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { books } from './books';
 //will import all reducers here as their own import line
+
 
 const allReducers = combineReducers({
   //w/e name you want: import name
+  books
 })
 
 export default allReducers;
