1a96798c644934ade0b49e78b5dafca573edc0fa
Stacy Potten <stacylynnaz@gmail.com>
2020-10-29 14:36:31 -0600

    Refactor reducer to unnest data

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -3,7 +3,7 @@ import { GET_BOOKLIST } from '../actions'
 export const books = (state = [], action) => {
   switch(action.type) {
     case GET_BOOKLIST:
-    return [...state, action.allBooks]
+    return action.allBooks
     default:
       return state
     }
