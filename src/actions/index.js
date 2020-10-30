8b8e6dcd2eb67d7a00cee7eb2b20977e9c2f17c6
Stacy Potten <stacylynnaz@gmail.com>
2020-10-29 19:35:50 -0600

    Add action to get new list name based on user input.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 export const GET_BOOKLIST = 'GET_BOOKLIST'
+export const GET_LISTNAME = 'GET_LISTNAME'
 
-export const getInitialBooks = allBooks => ({
+export const getNYTList = allBooks => ({
   type: GET_BOOKLIST,
   allBooks
+})
+
+export const getListName = listName => ({
+  type: GET_LISTNAME,
+  listName
 })
