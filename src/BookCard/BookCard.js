8ecd7429cf68693b3d179a3236aae92a002fabc6
Stacy Potten <stacylynnaz@gmail.com>
2020-10-29 21:09:35 -0600

    Add Button and onClick event for adding to toRead list

--- src/BookCard/BookCard.js
+++ src/BookCard/BookCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './BookCard.css';
 
-const BookCard = ({ bookInfo }) => {
+const BookCard = ({ bookInfo, addToToReadList } ) => {
   return (
     <div className='bookcard'>
+      <button className='to-read-btn' type='button' value={bookInfo.title} onClick={event => addToToReadList(event)}>+</button>
       <img className='bookcover' src={bookInfo.book_image} alt='front cover of book'/>
       <h5>Title: {bookInfo.title}</h5>
       <h5>Author: {bookInfo.author}</h5>
