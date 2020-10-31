3948ef0560837e47f694245a3680295e50b9e0f4
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-31 16:52:32 -0600

    Add reducer test filed with initial imports & test

---
+++ src/reducers/reducers.test.js
@@ -0,0 +1,15 @@
+import { books } from './books';
+import { haveRead } from './haveRead';
+import { list } from './list';
+import { toRead } from './toRead';
+import { userGoal } from './userGoal';
+
+import * as actions from '../actions';
+//import * as types from '../actions';
+
+
+describe('reducer', () => {
+  it('Should be hooked up properly', () => {
+    expect(true).toBe(true);
+  })
+})
