028bca23f3be8f99028d2df0ba20ac1280450405
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-31 15:52:25 -0600

    Add imports and initial test to set up BookContainer test file

---
+++ src/Components/BookContainer/BookContainer.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BookContainer from './BookContainer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('BookContainer', () => {
+  it('Should work', () => {
+    expect(true).toBe(true);
+  })
+})
