eb71629ac1a011937098f5f19fa00b40f09f966d
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-31 15:55:10 -0600

    Add imports and initial test for ToRead test file

---
+++ src/Components/ToRead/ToRead.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ToRead from './ToRead';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('ToRead', () => {
+  it('Should pass an initial test', () => {
+    expect(true).toBe(true);
+  })
+})
