7adece8c34e0f7a9a02b6fdca5a5b9e89b12410f
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-31 14:55:22 -0600

    Create GoalForm class

--- src/Components/GoalForm/GoalForm.js
+++ src/Components/GoalForm/GoalForm.js
@@ -1,4 +1,15 @@
 import React, { Component } from 'react';
 import './GoalForm.css';
 
+class GoalForm extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <h5>Form goes here</h5>
+    )
+  }
+}
 export default GoalForm;
