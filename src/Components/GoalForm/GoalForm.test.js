877b21a56441487d7eb15bf3894a56e0410c7365
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-31 15:53:24 -0600

    Add imports and initial test for GoalForm test file

---
+++ src/Components/GoalForm/GoalForm.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GoalForm from './GoalForm';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('GoalForm', () => {
+  it('Should pass an initial test', () => {
+    expect(true).toBe(true);
+  })
+})
