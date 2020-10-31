ead782707b3fa4847271c52fa9181602fbaa6c57
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-31 15:51:05 -0600

    Add initial imports and initial test to set up test file

---
+++ src/Components/BookCard/BookCard.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BookCard from './BookCard';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('BookCard', () => {
+  it('Should work', () => {
+    expect(true).toBe(true);
+  })
+})
