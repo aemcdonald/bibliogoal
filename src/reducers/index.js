f2c67cf41e8d5543370567cb64aef4ebe2ab50d0
Naomi Ware <nware1066@yahoo.com>
2020-10-31 10:56:42 -0600

    Import reducer and add to index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { books } from './books';
 import { listName } from './list';
 import { toReadList } from './toRead';
+import { haveReadList } from './haveRead';
 
 const allReducers = combineReducers({
   books,
   listName,
-  toReadList
+  toReadList,
+  haveReadList
 })
 
 export default allReducers;
