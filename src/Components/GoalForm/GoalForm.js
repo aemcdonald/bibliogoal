dddcf4dce2f26fda14ae9849f6fa3f06e3a4d803
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-10-31 15:24:26 -0600

    Add functionality set a user goal in state

--- src/Components/GoalForm/GoalForm.js
+++ src/Components/GoalForm/GoalForm.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createUserGoal } from '../../actions';
 import './GoalForm.css';
 
 class GoalForm extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+  }
+
+  handleUserInput = (event) => {
+    this.setState({ userGoal: event.target.value })
+  }
+
+  setUserGoal = (event) => {
+    this.props.createUserGoal(this.state.userGoal)
   }
 
   render() {
@@ -11,12 +21,26 @@ class GoalForm extends Component {
       <form>
         <input
           type='number'
+          min='1'
           placeholder='Add reading goal'
           name='userGoal'
+          onChange={event => this.handleUserInput(event)}
         />
-        <button type='button'>Submit</button>
+        <button type='button' onClick={this.setUserGoal}>Submit</button>
       </form>
     )
   }
 }
-export default GoalForm;
+
+const mapStateToProps = state => ({
+  userGoal: state.userGoal
+})
+
+const mapDispatchToProps = dispatch => ({
+  createUserGoal: userGoal => dispatch(createUserGoal(userGoal))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GoalForm)
