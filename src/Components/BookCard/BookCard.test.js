cb10018aceeec1d53d5a38cb8c93bd2f0243caf6
Naomi Ware <nware1066@yahoo.com>
2020-11-01 16:02:46 -0700

    Add initial test for BookCard

--- src/Components/BookCard/BookCard.test.js
+++ src/Components/BookCard/BookCard.test.js
@@ -2,9 +2,47 @@ import React from 'react';
 import BookCard from './BookCard';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from '../../reducers/index.js'
 
 describe('BookCard', () => {
-  it('Should work', () => {
-    expect(true).toBe(true);
+  it('render a book card', () => {
+
+const store = createStore(allReducers);
+// const allReducers = combineReducers({
+//   books,
+//   listName,
+//   toReadList,
+//   haveReadList,
+//   setUserGoal
+// })
+
+    const mockBook = {
+      book_image: 'mockURL',
+      title: 'mockTitle',
+      author: 'mockAuthor',
+      rank: '5'
+    }
+    const mockAddToReadList = jest.fn();
+    const mockAddToHaveReadList = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <BookCard
+          key={1}
+          bookInfo={mockBook}
+          addToToReadList={mockAddToReadList}
+          addToHaveReadList={mockAddToHaveReadList}
+        />
+      </ Provider>
+    )
+
+    expect(screen.getByText('Title: mockTitle')).toBeInTheDocument()
+    expect(screen.getByText('Author: mockAuthor')).toBeInTheDocument()
+    expect(screen.getByText('Rank: 5')).toBeInTheDocument()
+
+
+
   })
 })
