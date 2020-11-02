7510b4af05fbfd7a800643c4f0bc931675bf5b1d
Naomi Ware <nware1066@yahoo.com>
2020-11-01 19:23:54 -0700

    Add unit test for HaveRead Component

--- src/Components/HaveRead/HaveRead.test.js
+++ src/Components/HaveRead/HaveRead.test.js
@@ -2,9 +2,20 @@ import React from 'react';
 import HaveRead from './HaveRead';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from '../../reducers/index.js';
 
 describe('HaveRead', () => {
-  it('Should pass an initial test', () => {
-    expect(true).toBe(true);
+  it('display a message if list is empty', () => {
+
+    const store = createStore(allReducers);
+
+    render(
+      <Provider store={store}>
+        <HaveRead />
+      </Provider>
+    )
+    expect(screen.getByText('Your list is empty, add some books that you\'ve read!')).toBeInTheDocument()
   })
 })
