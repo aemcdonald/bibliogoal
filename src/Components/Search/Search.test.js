eaa4e15fda21a60166807f4e16edc522768cd6ab
Naomi Ware <nware1066@yahoo.com>
2020-11-01 19:49:07 -0700

    Create unit test for Search Component

--- src/Components/Search/Search.test.js
+++ src/Components/Search/Search.test.js
@@ -2,9 +2,26 @@ import React from 'react';
 import Search from './Search';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import allReducers from '../../reducers/index.js';
+
 
 describe('Search', () => {
-  it('Should pass an initial test', () => {
-    expect(true).toBe(true);
+  it('Should render a search form', () => {
+
+    const store = createStore(allReducers);
+
+    render(
+      <Provider store={store}>
+        <Search />
+      </Provider>
+    )
+
+    const dropDown = screen.getByRole('combobox')
+    expect(screen.getByText('Choose a List to Browse')).toBeInTheDocument()
+    expect(screen.getByRole('button', {name: 'Submit'})).toBeInTheDocument()
+    expect(screen.getByRole('combobox')).toBeInTheDocument()
+    expect(dropDown).toHaveLength(5)
   })
 })
