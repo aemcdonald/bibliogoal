c996bfb81a98918c77b209baa87cf4475d29d461
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-04 13:42:53 -0700

    Added action to update toRead list with updated array

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ export const GET_LISTNAME = 'GET_LISTNAME'
 export const ADD_TOREAD = 'ADD_TOREAD'
 export const ADD_TOHAVEREAD = 'ADD_TOHAVEREAD'
 export const CREATE_USERGOAL = 'CREATE_USERGOAL'
+export const UPDATE_TOREAD = 'UPDATE_TOREAD'
 
 export const getNYTList = allBooks => ({
   type: GET_BOOKLIST,
@@ -27,4 +28,9 @@ export const addToHaveRead = haveReadBook => ({
 export const createUserGoal = userGoal => ({
   type: CREATE_USERGOAL,
   userGoal
+})
+export const updateToRead = updatedList => ({
+  type: UPDATE_TOREAD,
+  updatedList
+  // toReadList: updatedList
 })
