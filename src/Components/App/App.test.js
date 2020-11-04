ebb41c1fa59f00c3de67a8ca2487458e7eb7a08b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-04 14:16:51 -0700

    Removed comments

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,6 +1,5 @@
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import App from './App';
-// import BookContainer from './BookContainer';
 import { createStore } from 'redux';
 import allReducers from '../../reducers/index.js';
 import React from 'react';
@@ -30,45 +29,7 @@ describe('App', () => {
     const submits = screen.getAllByText('Submit')
     expect(submits).toHaveLength(2)
     userEvent.click(submits[0])
-    
-    expect(screen.getByText('You have read 0 of 5 books on your list.')).toBeInTheDocument();
-  })
 
-  // it('Should allow the user to select a new list of books', async () => {
-  //
-  //   getBooks.mockResolvedValue({
-  //     results: {
-  //       books: [
-  //         {
-  //           book_image: 'mockURL',
-  //           title: 'mockTitle1',
-  //           author: 'mockAuthor1',
-  //           rank: 5
-  //         }
-  //       ]
-  //     }
-  //   })
-  //
-  //   render(
-  //     <Provider store={store}>
-  //       <BrowserRouter>
-  //         <App />
-  //       </BrowserRouter>
-  //     </Provider>
-  //   )
-  //
-  //   expect(screen.getByText('Hardcover Fiction')).toBeInTheDocument()
-  //   userEvent.click(screen.getByText('Choose a List to Browse'))
-  //   fireEvent.change(screen.getByRole('combobox'), {target: {value: 'Crime and Punishment'}})
-  //   expect(screen.getByRole('combobox')).toHaveValue('Crime and Punishment')
-  //   // userEvent.click(screen.getByText('Crime and Punishment'))
-  //   // expect(screen.getByRole('combobox')).toHaveValue('Crime and Punishment')
-  //   userEvent.click(screen.getByTestId('search-button'))
-  //   const bookList = await waitFor(() => {
-  //     screen.getByTestId('list-title')
-  //   })
-  //   screen.debug();
-  //
-  //   expect(bookList).toBe('Crime and Punishment')
-  // })
+    expect(screen.getByText('You have read 0 of 5 books on your list.')).toBeInTheDocument();
+  });
 });
