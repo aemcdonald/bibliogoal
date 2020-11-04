d12033b64af1f88cc4361c83bf53396cd55ba179
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-04 13:42:10 -0700

    Added logic to remove a book from the to read list

--- src/Components/ToRead/ToRead.js
+++ src/Components/ToRead/ToRead.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addToHaveRead } from '../../actions';
+import { addToHaveRead, updateToRead } from '../../actions';
 import BookCard from '../BookCard/BookCard';
 import PropTypes from 'prop-types';
 import './ToRead.css';
@@ -12,6 +12,19 @@ const ToRead = ({ books, toReadList, addToHaveRead }) => {
       return book.title === event.target.value
     })
     addToHaveRead(readBook)
+    //gets added to HaveReadList
+    //method:
+      //if book is on HaveReadList AND toReadList
+        //remove book from toReadList
+
+    if (toReadList.some(book => book.title === readBook.title)) {
+      const updatedList = toReadList.filter(book => {
+        return book.title !== readBook.title
+      })
+      console.log(updatedList);
+      //call action here:
+      updateToRead(updatedList)
+    }
   }
 
   const displayToReadBooks = () => {
@@ -42,10 +55,12 @@ const mapStateToProps = state => ({
   books: state.books,
   toReadList: state.toReadList,
   haveReadList: state.haveReadList
+  // updatedList: state.updatedList?
 })
 
 const mapDispatchToProps = dispatch => ({
-  addToHaveRead: readBook => dispatch(addToHaveRead(readBook))
+  addToHaveRead: readBook => dispatch(addToHaveRead(readBook)),
+  updateToRead: updatedList => dispatch(updateToRead(updatedList))
 })
 
 export default connect(
@@ -57,3 +72,22 @@ ToRead.propTypes = {
   toReadList: PropTypes.array,
   haveReadList: PropTypes.array
 }
+
+//if haveRead button clicked
+  //should remove book from to read list in state
+
+// const removeBook = (event) => {
+//   const foundBookToRemove = toReadList.find(book => {
+//     return book.title === event.target.value
+//   })
+//   if (foundBookToRemove) {
+//     const updatedList = toReadList.filter(book => {
+//       return book.title !== event.target.value
+//     })
+//   }
+//   //call action here:
+//   console.log(updatedList);
+//   updateToRead(updatedList)
+// }
+
+//pass delete functionality to bookCard
