669c9d20a75297eef12d09b208477a612fadf03b
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-04 13:43:20 -0700

    Attempt to fix bug for updating to read list after removing book.

--- src/reducers/toRead.js
+++ src/reducers/toRead.js
@@ -1,10 +1,38 @@
 import { ADD_TOREAD } from '../actions'
+import { UPDATE_TOREAD } from '../actions'
 
 export const toReadList = (state = [], action) => {
   switch(action.type) {
     case ADD_TOREAD:
+      console.log("TO READ!");
       return [...state, action.toReadBook]
+      break;
+    case UPDATE_TOREAD:
+      console.log("switch fired");
+      return [...state, action.updatedList]
     default:
       return state
   }
+
+  // switch(action.type) {
+  //   case ADD_TOREAD: {
+  //     console.log("TO READ!");
+  //     return [...state, action.toReadBook]
+  //     break;
+  //   }
+  //
+  //   case UPDATE_TOREAD: {
+  //     console.log("switch fired");
+  //     return [...state, action.updatedList]
+  //   }
+  //   default:
+  //     return state
+  // }
+
+  // switch(action.type) {
+  //   case UPDATE_TOREAD:
+  //     return [action.updatedList]
+  //   default:
+  //     return state
+  // }
 }
