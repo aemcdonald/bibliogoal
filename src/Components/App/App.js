973413b8b6b3cd9e80eece3e3ed950d2dabaa7ba
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-04 15:18:33 -0700

    Add sections for styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,16 +12,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <main>
+      <main className="main-area">
         <h1 className='title'>BiblioGoal</h1>
-        <nav className='links'>
-          <Link className='homelink' to='/'>Home</Link> || <Link className='have-read-link' to='/have-read'>Books I Have Read</Link> || <Link className='to-read-link' to='/to-read'>Books To Read</Link>
-        </nav>
-        <GoalForm />
-        {this.props.userGoal &&
-        <p className='progress-text'>You have read {this.props.haveReadList.length} of {this.props.userGoal} books on your list.</p>}
-        {this.props.userGoal && this.props.haveReadList.length >= this.props.userGoal &&
-        <p className='progress-text'>Congratulations! You've met your reading goal!</p>}
+        <section className='nav-area'>
+          <nav className='links'>
+            <Link className='homelink' to='/'>Home</Link> || <Link className='have-read-link' to='/have-read'>Books I Have Read</Link> || <Link className='to-read-link' to='/to-read'>Books To Read</Link>
+          </nav>
+          <section className='goal-form-area'>
+            <GoalForm />
+            {this.props.userGoal &&
+            <p className='progress-text'>You have read {this.props.haveReadList.length} of {this.props.userGoal} books on your list.</p>}
+            {this.props.userGoal && this.props.haveReadList.length >= this.props.userGoal &&
+            <p className='progress-text'>Congratulations! You've met your reading goal!</p>}
+          </section>
+        </section>
         <Route exact path='/'>
           <Search />
           <BookContainer />
